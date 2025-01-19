537f10b60b275ffff60e535ab3fd08ded2f979de
Jason McNeil <sixcolors@mac.com>
2025-01-19 14:24:07 -0400

    test(App): add component rendering test and update dependencies

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -7,8 +7,10 @@ describe('App.svelte', () => {
   // TODO: @testing-library/svelte claims to add this automatically but it doesn't work without explicit afterEach
   afterEach(() => cleanup());
 
+  it('should render the component correctly', () => {
     const { container } = render(App as unknown as new () => SvelteComponent, { target: document.body });
     expect(container).toBeTruthy();
     expect(container.innerHTML).toContain('Canadian Wildfire Map');
     expect(container.innerHTML).toMatchSnapshot();
+  });
 });
